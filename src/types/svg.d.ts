ed6baebf7117c12eb5af49fda86f82b9f809a833
J S <smutek.jakub@seznam.cz>
2026-02-06 21:58:16 +0100

    Updates

--- src/types/svg.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*.svg?component' {
-  import * as React from 'react';
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
